8b13ff8a5af9b48e57bf45414d6984387c35fc73
boyko_dmytro <boyko.dmtr@gmail.com>
2023-10-25 14:03:36 +0300

    create implementation of service

--- src/main/java/com/binance/services/BTCUSDTService.java
+++ src/main/java/com/binance/services/BTCUSDTService.java
@@ -3,7 +3,6 @@ package com.binance.services;
 import com.binance.responses.BTCUSD_PERP;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface BTCUSDTService {
     BTCUSD_PERP getOrderBook();
 }
--- src/main/java/com/binance/services/BTCUSDTServiseImpl.java
+++ src/main/java/com/binance/services/BTCUSDTServiseImpl.java
@@ -1,10 +1,12 @@
 package com.binance.services;
 
 import com.binance.responses.BTCUSD_PERP;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BTCUSDTServiseImpl implements BTCUSDTService {
     @Override
     public BTCUSD_PERP getOrderBook() {
-        return null;
+        return new BTCUSD_PERP();
     }
 }
