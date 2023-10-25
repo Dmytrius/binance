6d68ecbca4aaedf64b53a4e514163101b306669f
boyko_dmytro <boyko.dmtr@gmail.com>
2023-10-25 14:03:15 +0300

    create response body

--- src/main/java/com/binance/responses/BTCUSD_PERP.java
+++ src/main/java/com/binance/responses/BTCUSD_PERP.java
@@ -1,4 +1,21 @@
 package com.binance.responses;
 
+import com.binance.enums.*;
+import com.binance.enums.Symbol;
+import lombok.Data;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Data
 public class BTCUSD_PERP {
+
+    Date lastUpdateId;
+    Symbol symbol;
+    Pair pair;
+    Date E;
+    Date T;
+    ArrayList<AbstractMap.SimpleEntry<String, String>> bids;
+    ArrayList<AbstractMap.SimpleEntry<String, String>> asks;
 }
