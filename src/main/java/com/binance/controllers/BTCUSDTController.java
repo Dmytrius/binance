afe8958c0b16916baa8311a1ada1cd658892ca7a
boyko_dmytro <boyko.dmtr@gmail.com>
2023-10-25 12:24:00 +0300

    create request endpoint

--- src/main/java/com/binance/controllers/BTCUSDTController.java
+++ src/main/java/com/binance/controllers/BTCUSDTController.java
@@ -1,17 +1,20 @@
 package com.binance.controllers;
 
 import com.binance.responses.BTCUSD_PERP;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BTCUSDTController {
 
-    @GetMapping(path = "/dapi/v1/depth")
+    @GetMapping(path = "/dapi/v1/depth",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public BTCUSD_PERP getOrderBook(
-            @PathVariable String symbol,
-            @PathVariable Integer limit
+            @RequestParam String symbol,
+            @RequestParam(required = false, defaultValue = "500") String limit
             ){
         return new BTCUSD_PERP();
     }
