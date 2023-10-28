693f43ba70e168e98b03a2fa5509642e3c3deebb
boyko_dmytro <boyko.dmtr@gmail.com>
2023-10-28 23:12:32 +0300

    insert json property to response

--- src/main/java/com/binance/responses/BTCUSD_PERP.java
+++ src/main/java/com/binance/responses/BTCUSD_PERP.java
@@ -1,21 +1,28 @@
 package com.binance.responses;
 
-import com.binance.enums.*;
+import com.binance.enums.Pair;
 import com.binance.enums.Symbol;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Date;
 
 @Data
 public class BTCUSD_PERP {
 
-    Date lastUpdateId;
+
+    Integer lastUpdateId;
+    @JsonProperty("s")
     Symbol symbol;
+    @JsonProperty("ps")
     Pair pair;
+    @JsonProperty("E")
     Date E;
+    @JsonProperty("T")
     Date T;
-    ArrayList<AbstractMap.SimpleEntry<String, String>> bids;
-    ArrayList<AbstractMap.SimpleEntry<String, String>> asks;
+    @JsonProperty("b")
+    ArrayList<ArrayList<String>> bids;
+    @JsonProperty("a")
+    ArrayList<ArrayList<String>> asks;
 }
