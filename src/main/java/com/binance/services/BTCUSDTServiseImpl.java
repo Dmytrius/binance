37c53f92e41b1b14e52c6727561e36c12f6aedb8
boyko_dmytro <boyko.dmtr@gmail.com>
2023-10-29 19:13:33 +0200

    change logic into controller

--- src/main/java/com/binance/services/BTCUSDTServiseImpl.java
+++ src/main/java/com/binance/services/BTCUSDTServiseImpl.java
@@ -1,13 +1,30 @@
 package com.binance.services;
 
+import com.binance.entities.BTCUSDT;
 import com.binance.responses.BTCUSD_PERP;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BTCUSDTServiseImpl implements BTCUSDTService {
 
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    private ObjectMapper mapper;
     @Override
     public BTCUSD_PERP getOrderBook() {
-        return new BTCUSD_PERP();
+        BTCUSDT btcusdt = context.getBean(BTCUSDT.class);
+        BTCUSD_PERP response;
+        try {
+            response = mapper.readValue(btcusdt.message, BTCUSD_PERP.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        response.setLastUpdateId(btcusdt.id);
+        return response;
     }
 }
